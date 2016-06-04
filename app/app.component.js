b21c67c1d28fc515255bdeb2b765d0dd96622fa6
Paul <paullehenaff@msn.com>
2016-06-04 19:49:49 +0100

    Initial commit

---
+++ app/app.component.js
@@ -0,0 +1,32 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+        this.counter = 16;
+    }
+    Object.defineProperty(AppComponent.prototype, "message", {
+        get: function () {
+            if (this.counter > 0) {
+                return this.counter + " taps left";
+            }
+            else {
+                return "Hoorraaay! \nYou are ready to start building!";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AppComponent.prototype.onTap = function () {
+        this.counter--;
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: "my-app",
+            template: "\n<StackLayout>\n    <Label text=\"Tap the button\" class=\"title\"></Label>\n    \n    <Button text=\"TAP\" (tap)=\"onTap()\"></Button>\n\n    <Label [text]=\"message\" class=\"message\" textWrap=\"true\"></Label>\n</StackLayout>\n",
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
